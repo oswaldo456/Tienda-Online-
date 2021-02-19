4d24845cbd5e2e75f83e29d5daf2cddf0e65a32d
oswaldo456 <osvamosa@hotmail.com>
2021-02-18 21:49:51 -0600

    agregamos el app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/Categorias', (req, res) => {
+return res.send('Este servicio muestra las categorias');
+});
+app.get('/DetallesOrdenes', (req, res) => {
+return res.send('Este servicio muestra la informacion de la orden');
+});
+app.get('/Productos', (req, res) => {
+return res.send('Este servicio muestra los productos');
+});
+app.post('/Crear cuenta', (req, res) => {
+return res.send('Servicio de crear cuenta');
+});
+app.post('/Crear ordenes', (req, res) => {
+return res.send('Este servicio crea ordenes');
+});
+app.delete('/Borrar orden', (req, res) => {
+return res.send('Este servicio elimina las ordenes');       
+});
+app.put('/', (req, res) => {
+return res.send('Received a PUT HTTP method');
+});
